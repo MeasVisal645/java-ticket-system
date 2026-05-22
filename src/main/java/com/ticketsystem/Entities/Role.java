39c6304e210cc899e45e54e4c38ef07cd084cd9e
MeasVisal645 <measvathanakvisal@gmail.com>
2026-05-22 13:35:54 +0700

    Create User Model

---
+++ src/main/java/com/ticketsystem/Entities/Role.java
@@ -0,0 +1,18 @@
+package com.ticketsystem.Entities;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    ADMIN("ADMIN"),
+    USER("USER"),
+    CUSTOMER("CUSTOMER"),
+    MANAGER("MANAGER"),
+    AGENT("AGENT");
+
+    private final String value;
+
+    Role(String value){
+        this.value = value;
+    };
+}
---
+++ src/main/java/com/ticketsystem/Entities/User.java
@@ -0,0 +1,44 @@
+package com.ticketsystem.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table("user")
+public class User {
+
+    public static final String LABEL =  "user";
+    public static final String ID_COLUMN = "id";
+    public static final String USERNAME_COLUMN = "username";
+    public static final String PASSWORD_COLUMN = "password";
+    public static final String ROLE_COLUMN = "role";
+    public static final String IS_ACTIVE_COLUMN = "isActive";
+
+    @Id
+    @Column(ID_COLUMN)
+    private Long id;
+    @Column(USERNAME_COLUMN)
+    private String username;
+    @Column(PASSWORD_COLUMN)
+    private String password;
+    @Column(ROLE_COLUMN)
+    private Role role;
+    @Column(IS_ACTIVE_COLUMN)
+    private boolean isActive;
+
+    public static UserBuilder from(User user) {
+        return User.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .isActive(user.isActive());
+    }
+}
