2a4b50f90ca80bea9271663651eaab82f11a25d6
MeasVisal645 <measvathanakvisal@gmail.com>
2026-05-22 13:39:38 +0700

    Create User Service

---
+++ src/main/java/com/ticketsystem/Service/UserService.java
@@ -0,0 +1,14 @@
+package com.ticketsystem.Service;
+
+import com.ticketsystem.Dto.AuthRequest;
+import com.ticketsystem.Dto.AuthResponse;
+import com.ticketsystem.Entities.User;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public interface UserService {
+    Mono<User> create(User user);
+    Mono<AuthResponse> signIn(AuthRequest request);
+    Mono<AuthResponse> refreshToken(String token);
+}
---
+++ src/main/java/com/ticketsystem/ServiceImpl/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package com.ticketsystem.ServiceImpl;
+
+import com.ticketsystem.Dto.AuthRequest;
+import com.ticketsystem.Dto.AuthResponse;
+import com.ticketsystem.Entities.Role;
+import com.ticketsystem.Entities.User;
+import com.ticketsystem.Repository.UserRepository;
+import com.ticketsystem.Service.UserService;
+import com.ticketsystem.Utils.JwtUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+
+    @Override
+    public Mono<AuthResponse> signIn(AuthRequest request) {
+        if (request.username() == null || request.username().isBlank()
+                || request.password() == null || request.password().isBlank()) {
+            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username or password is required"));
+        }
+
+        return userRepository.findByUsername(request.username())
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")))
+                .flatMap(user -> {
+
+                    if (!user.isActive()) {
+                        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "User account is inactive"));
+                    }
+
+                    if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+                        return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials"));
+                    }
+
+                    String access = jwtUtils.generateAccessToken(user);
+                    String refresh = jwtUtils.generateRefreshToken(user);
+
+                    return Mono.just(new AuthResponse(access, refresh));
+                });
+    }
+
+    @Override
+    public Mono<User> create(User user) {
+        var encodedPassword = passwordEncoder.encode(user.getPassword());
+
+        return userRepository.save(
+                User.from(user)
+                        .role(Role.USER)
+                        .password(encodedPassword)
+                        .isActive(true)
+                        .build()
+        );
+    }
+
+    @Override
+    public Mono<AuthResponse> refreshToken(String token) {
+        if (token == null || token.isBlank()) {
+            return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Refresh token is required"));
+        }
+        if (!jwtUtils.validateToken(token) || !jwtUtils.isRefreshToken(token)) {
+            return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid refresh token"));
+        }
+
+        String newAccessToken = jwtUtils.refreshAccessToken(token);
+
+        return Mono.just(new AuthResponse(newAccessToken, null));
+    }
+
+}
