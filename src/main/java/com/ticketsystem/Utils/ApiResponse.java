d071213485c590000500cdc9d48247b4ce0f8916
MeasVisal645 <measvathanakvisal@gmail.com>
2026-05-29 13:41:41 +0700

    Update ApiResponse

--- src/main/java/com/ticketsystem/Utils/ApiResponse.java
+++ src/main/java/com/ticketsystem/Utils/ApiResponse.java
@@ -7,4 +7,44 @@ public record ApiResponse<T>(
         String message,
         T data
 ) {
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(
+                HttpStatus.OK,
+                "Success",
+                data
+        );
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(
+                HttpStatus.OK,
+                message,
+                data
+        );
+    }
+
+    public static ApiResponse<Void> deleted(String message) {
+        return new ApiResponse<>(
+                HttpStatus.OK,
+                message,
+                null
+        );
+    }
+
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(
+                HttpStatus.CREATED,
+                "Created Success",
+                data
+        );
+    }
+
+    public static <T> ApiResponse<T> updated(T data) {
+        return new ApiResponse<>(
+                HttpStatus.OK,
+                "Updated Success",
+                data
+        );
+    }
 }
