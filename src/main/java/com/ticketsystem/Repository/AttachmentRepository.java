b285c81ce595df78936b1024e146ca0df7985bf8
MeasVisal645 <measvathanakvisal@gmail.com>
2026-06-03 07:53:55 +0700

    Add delete by ticket id

--- src/main/java/com/ticketsystem/Repository/AttachmentRepository.java
+++ src/main/java/com/ticketsystem/Repository/AttachmentRepository.java
@@ -3,7 +3,10 @@ package com.ticketsystem.Repository;
 import com.ticketsystem.Entities.Attachment;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface AttachmentRepository extends R2dbcRepository<Attachment, Long> {
+
+    Mono<Void> deleteByTicketId(Long ticketId);
 }
