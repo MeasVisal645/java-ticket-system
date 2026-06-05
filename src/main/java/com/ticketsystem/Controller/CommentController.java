1ff98a2685e99e6438f8b1c6d6ff81f52fdd9830
MeasVisal645 <measvathanakvisal@gmail.com>
2026-06-05 08:17:23 +0700

    Add Comment Controller

---
+++ src/main/java/com/ticketsystem/Controller/CommentController.java
@@ -0,0 +1,41 @@
+package com.ticketsystem.Controller;
+
+import com.ticketsystem.Dto.CommentDto;
+import com.ticketsystem.Service.CommentService;
+import com.ticketsystem.Utils.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/ticket-comment")
+@AllArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/{id}")
+    public Mono<ApiResponse<CommentDto>> findById(@PathVariable Long id) {
+        return commentService.findById(id)
+                .map(ApiResponse::success);
+    }
+
+    @PostMapping("/create")
+    public Mono<ApiResponse<?>> create(@Valid @RequestBody CommentDto commentDto) {
+        return commentService.create(commentDto)
+                .map(ApiResponse::created);
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<ApiResponse<?>> update(@Valid @RequestBody CommentDto commentDto) {
+        return commentService.update(commentDto)
+                .map(ApiResponse::success);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<ApiResponse<?>> delete(@PathVariable Long id) {
+        return commentService.delete(id)
+                .thenReturn(ApiResponse.deleted("Delete Success"));
+    }
+}
