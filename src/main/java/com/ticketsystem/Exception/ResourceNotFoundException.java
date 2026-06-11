c22ba006a38761edb1eb59d94502d6a8a8f50809
MeasVisal645 <measvathanakvisal@gmail.com>
2026-06-11 19:10:03 +0700

    Create Exception

---
+++ src/main/java/com/ticketsystem/Exception/ResourceNotFoundException.java
@@ -0,0 +1,22 @@
+package com.ticketsystem.Exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends ApiException {
+
+    public ResourceNotFoundException(String resourceName, String id) {
+        super(
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
+                String.format("%s with id: %s not found", resourceName, id)
+        );
+    }
+
+    public ResourceNotFoundException(String resourceName) {
+        super(
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
+                String.format("%s not found", resourceName)
+        );
+    }
+}
---
+++ src/main/java/com/ticketsystem/Exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.ticketsystem.Exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
