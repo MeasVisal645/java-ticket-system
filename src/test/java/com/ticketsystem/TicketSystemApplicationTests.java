04a23ab7b2fdf3ee4a23b062907c928da634d5e7
MeasVisal645 <measvathanakvisal@gmail.com>
2026-06-11 10:34:58 +0700

    minor commit

--- src/test/java/com/ticketsystem/TicketSystemApplicationTests.java
+++ src/test/java/com/ticketsystem/TicketSystemApplicationTests.java
@@ -1,13 +1,23 @@
 package com.ticketsystem;
 
+import com.ticketsystem.Configuration.DotEnvConfig;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TicketSystemApplication.class)
+@ConfigurationPropertiesScan
 class TicketSystemApplicationTests {
 
     @Test
     void contextLoads() {
+        DotEnvConfig dotEnvConfig = new DotEnvConfig();
+        Dotenv dotenv = dotEnvConfig.dotenv();
+
+        dotenv.entries().forEach(
+                entry -> System.setProperty(entry.getKey(), entry.getValue())
+        );
     }
 
 }
