b6b155bbea26398382488f672c3aae201125a7ad
MeasVisal645 <measvathanakvisal@gmail.com>
2026-06-12 14:11:29 +0700

    All query for max ticketNo

--- src/main/java/com/ticketsystem/Repository/TicketRepository.java
+++ src/main/java/com/ticketsystem/Repository/TicketRepository.java
@@ -1,9 +1,14 @@
 package com.ticketsystem.Repository;
 
 import com.ticketsystem.Entities.Ticket;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface TicketRepository extends R2dbcRepository<Ticket, Long> {
+
+    @Query("SELECT ticketNo FROM ticket ORDER BY id DESC LIMIT 1")
+    Mono<String> findMaxTicketNo();
 }
