94049a5e2771797c684a01bdcf81f00b24615ddb
MeasVisal645 <measvathanakvisal@gmail.com>
2026-08-12 08:06:49 +0700

    minor change

--- src/main/java/com/ticketsystem/Configuration/SecurityConfig.java
+++ src/main/java/com/ticketsystem/Configuration/SecurityConfig.java
@@ -51,8 +51,6 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
                         .pathMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .pathMatchers("/api/v1/auth/**").permitAll()
-//                        .pathMatchers("/api/v1/ticket-history/latest").permitAll()
-//                        .pathMatchers("/api/v1/ticket-history/max-id").permitAll()
                         .pathMatchers(
                                 "/v3/api-docs/**",
                                 "/scalar.html"
